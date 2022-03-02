4fc3d991803ab216f56ad1b911990ec213cb2960
Jeremie Deletraz <jeremie.deletraz@open-groupe.com>
2022-03-02 15:13:22 +0100

    initial commit

---
+++ sound-bdd/sound-bdd.js
@@ -0,0 +1,91 @@
+const objects = [  
+    {
+        "label":"Je vais le faire",
+        "path":"je-vais-faire.mp3"
+    },
+    {
+        "label":"OSS 117 - rire 1",
+        "path":"oss-117-rire-1.mp3"
+    },
+    {
+        "label":"Merci la gueuse",
+        "path":"les-visiteurs-merci.mp3"
+    }, 
+    {
+        "label":"C'est une catastrophe !",
+        "path":"les-visiteurs-catastrophe.mp3"
+    }, 
+    {
+        "label":"Okay !",
+        "path":"les-visiteurs-ok.mp3"
+    },
+    {
+        "label":"Il s'appelle Juste Leblanc",
+        "path":"juste.mp3"
+    },
+    {
+        "label":"Une ouiche lorraine",
+        "path":"ouiche-lorraine.mp3"
+    },
+    {
+        "label":"Bon maintenant t'arrêtes !",
+        "path":"la-megachiasse.mp3"
+    },
+    {
+        "label":"Un fasciste de merde",
+        "path":"fasciste-de-merde.mp3"
+    },
+    {
+        "label":"Des chips",
+        "path":"chips.mp3"
+    },
+    {
+        "label":"Terrorisme, grand banditisme",
+        "path":"terrorisme.mp3"
+    },
+    {
+        "label":"Mobilisation générale à Rennes",
+        "path":"stup-rennes.mp3"
+    },
+    {
+        "label":"La moindre erreur ne pardonne pas",
+        "path":"erreur.mp3"
+    },
+    {
+        "label":"AH !",
+        "path":"denis-brogniart-ah.mp3"
+    },
+    {
+        "label":"Ouille ouille ouille",
+        "path":"ouille-ouille.mp3"
+    },
+    {
+        "label":"La chatte de Marine",
+        "path":"marine.mp3"
+    },
+    {
+        "label":"Au revoir",
+        "path":"au-revoir.mp3"
+    },
+    {
+        "label":"C'est contre nature",
+        "path":"contre-nature.mp3"
+    },
+    {
+        "label":"Mais t'es pas net !",
+        "path":"tes-pas-net.mp3"
+    },
+    {
+        "label":"New York Unité Spéciale",
+        "path":"law-order.mp3"
+    },
+    {
+        "label":"Ah bah bravo Nils !",
+        "path":"ah-bah-bravo-nils.mp3"
+    },
+    {
+        "label":"Tu es grosse Melissandre",
+        "path":"tu-es-grosse-melissandre.mp3"
+    } 
+    
+]
